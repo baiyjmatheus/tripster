d4f422273e985f4d87b501289294a966b2c9032c
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-01-25 21:45:43 +0000

    intital summary page setup

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var webpack = require('webpack');
+node var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
--- server/index.js
+++ server/index.js
@@ -8,12 +8,17 @@ const ENV = process.env.ENV || "development";
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig[ENV]);
 const knexLogger = require('knex-logger');
+const path = require('path'); //added this package b/c matt said it wouldnt work otherwise
 
 app.use(knexLogger(knex));
 
 app.get("/", (req, res) => {
   res.send('Hello server');
 });
+
+app.get("/summary", (req, res) => {
+  res.sendFile(path.join(__dirname + '/summary.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening to ${PORT}`);
