56ae785a6938d8462f4f58843b840ad376a08be3
Matheus Bai <baiyj.matheus@gmail.com>
2019-01-25 21:43:41 +0000

    new user query

--- server/index.js
+++ server/index.js
@@ -10,16 +10,37 @@ const knex = require('knex')(knexConfig[ENV]);
 const knexLogger = require('knex-logger');
 
 const path = require('path');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(knexLogger(knex));
 app.use(express.static('public'));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['final']
+}));
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname + '/login.html'));
 });
 
 app.post('/login', (req, res) => {
-  res.redirect('http://localhost:3000');
+  const { email, name } = req.body;
+  knex.select('id','email', 'name').from('users').where('email', email)
+    .then((existingUser) => {
+      if (existingUser.length === 0) {
+        knex('users').returning('id').insert({email, name})
+        .then((newUserId) => {
+          req.session.id = newUserId[0];
+        });
+      } else {
+        req.session.id = existingUser.id;
+      }
+      res.redirect('http://localhost:3000');
+    })
+  
+  
 });
 
 app.listen(PORT, () => {
