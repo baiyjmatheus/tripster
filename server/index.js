2830e05fabc871bb2ec6fd45b8aadce31805db8d
MikeEngerer <michael.engerer@hotmail.com>
2019-01-28 18:59:44 +0000

    color now set in a cycle

--- server/index.js
+++ server/index.js
@@ -67,17 +67,16 @@ app.post('/trips', (req, res) => {
 
 // on client connect/disconnect, socket is created/destroyed
 io.on('connection', socket => {
-  let colorsIncrement = 0
 	console.log('new socket established', socket.id);
   socket.on('new user', userId => {
     knex('users').returning('*').where('id', userId).then(user => {
       const userData = {
         id: user[0].id,
         name: user[0].name,
-        color: setUserColor(colorsIncrement)
+        color: setUserColor(socket.conn.server.clientsCount % 3)
       }
       socket.emit('new user', userData)
-      colorsIncrement == 2 ? colorsIncrement = 0 : colorsIncrement++
+      // colorsIncrement == 2 ? colorsIncrement = 0 : colorsIncrement++
     })
   })
   // console.log('session:', session)
