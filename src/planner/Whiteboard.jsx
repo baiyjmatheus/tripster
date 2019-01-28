c4585cb9d863969b8d84a341947626ab0d1893d3
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-01-28 23:58:43 +0000

    setting up basic components for whiteboard on the same file

--- src/planner/Whiteboard.jsx
+++ src/planner/Whiteboard.jsx
@@ -8,8 +8,31 @@ class Whiteboard extends Component {
           <h1>Status bar</h1>
         </div>
         <hr />
+        <div>
+          < Flight />
+        </div>
       </main>
     );
+  }
+}
+
+class Flight  extends Component {
+  render () {
+    return (
+      <div>
+        <h1> this is the flights page </h1>
+      </div>
+    )
+  }
+}
+
+class Hotels  extends Component {
+  render () {
+    return (
+      <div>
+        <h1> this is the hotels page </h1>
+      </div>
+    )
   }
 }
 
