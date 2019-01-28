5800a602fecc4b95ccd2dda30c06a1fc4a46d4fa
Matheus Bai <baiyj.matheus@gmail.com>
2019-01-28 22:01:19 +0000

    Redirect to pages with the right syntax

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,43 +1,55 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <main id="login-container">
+    const { redirect } = this.state;
+
+     if (redirect) {
+       return (<Redirect to='/trips'/>);
+     } else {
+      return (
+        <div>
+          <main id="login-container">
+        
+          <section id="login">
+            <h1>Get started</h1>
+            <form action="/login" method="POST" onSubmit={ this.handleLogin } >
+              <input type="email" name="email" placeholder="Email"/>
+              <input type="text" name="name" placeholder="Name"/>
+              <button>Login</button>
+            </form>
+          </section>
       
-        <section id="login">
-          <h1>Get started</h1>
-          <form action="/login" method="POST" onSubmit={ this.handleLogin } >
-            <input type="email" name="email" placeholder="Email"/>
-            <input type="text" name="name" placeholder="Name"/>
-            <button>Login</button>
-          </form>
-        </section>
+          <aside id="info">
+            <header>
+              <img class="logo-header" src="/img/travel.png"/>
+              <h1>We are Tripster</h1>
+            </header>
+              
+              <h3 class="catchphrase">Travel together is always better</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias officia sequi optio laudantium minus ea dolores quisquam. Ut at officia enim commodi, molestiae repellendus facilis tempore aut, suscipit error debitis.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, totam. Delectus odio natus qui nobis facere at, laudantium veritatis et ullam. Distinctio dignissimos minima sint provident voluptatem harum, sequi incidunt.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, totam. Delectus odio natus qui nobis facere at, laudantium veritatis et ullam. Distinctio dignissimos minima sint provident voluptatem harum, sequi incidunt.</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus officia accusantium voluptatem sunt enim incidunt iure quo quibusdam odio, inventore, quos quae, facilis iste praesentium est labore sint delectus consequuntur.</p>
+            </aside>
     
-        <aside id="info">
-          <header>
-            <img class="logo-header" src="/img/travel.png"/>
-            <h1>We are Tripster</h1>
-          </header>
-            
-            <h3 class="catchphrase">Travel together is always better</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias officia sequi optio laudantium minus ea dolores quisquam. Ut at officia enim commodi, molestiae repellendus facilis tempore aut, suscipit error debitis.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, totam. Delectus odio natus qui nobis facere at, laudantium veritatis et ullam. Distinctio dignissimos minima sint provident voluptatem harum, sequi incidunt.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, totam. Delectus odio natus qui nobis facere at, laudantium veritatis et ullam. Distinctio dignissimos minima sint provident voluptatem harum, sequi incidunt.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus officia accusantium voluptatem sunt enim incidunt iure quo quibusdam odio, inventore, quos quae, facilis iste praesentium est labore sint delectus consequuntur.</p>
-          </aside>
-  
-        </main>
-      </div>
-    );
+          </main>
+        </div>
+      )}
   }
 
   handleLogin = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const user = {
       name: e.target.name.value,
       email: e.target.email.value
@@ -46,9 +58,10 @@ class Login extends Component {
       .then((res) => {
         const cookies = new Cookies();
         cookies.set('user_id', res.data.id);
-        window.location.replace(`/trips/`)
-      })
-  }  
+        // <Route exact path="/" render={() => (<Redirect to="/trips"/>)} />
+        this.setState({ redirect: true });
+      }); 
+  }
 }
 
 export default Login;
