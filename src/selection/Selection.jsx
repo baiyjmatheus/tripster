7584f132e68a0a08580ea6c066c46d32b7de5392
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-01-28 00:45:00 +0000

    set up post but still need to query

--- src/selection/Selection.jsx
+++ src/selection/Selection.jsx
@@ -13,11 +13,11 @@ class Selection extends Component {
         }
     }
     return (
-      
+
 
       <main id="selection-container" style={backgroundStyle('https://images.unsplash.com/photo-1484544808355-8ec84e534d75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1966&q=80')}>
         <aside id="selection-create" style={{backgroundColor: 'transparent'}}>
-          <form action="/trips" method="POST" onSubmit={this.createTrip}>
+          <form action="/trips/create" method="POST" onSubmit={this.createTrip}>
 
             <div id="date-selection">
               <input type="date" name="start" placeholder='Start Date'/>
@@ -29,10 +29,13 @@ class Selection extends Component {
             <button>Create</button>
           </form>
         </aside>
-        
+
         <section id="selection-join" style={{backgroundColor: 'transparent'}}>
-          <input type="text" placeholder='Trip Code'/>
-          <Link to='/trips/join'><button>Join</button></Link>
+          <form action="/trips/join"  method= "POST" onSubmit={this.joinTrip}>
+            <input type="text" name="code" placeholder='Trip Code'/>
+            {/*<Link to='/trips/join'><button>Join</button></Link> */}
+            <button> Join </button>
+          </form>
         </section>
       </main>
     );
@@ -47,11 +50,35 @@ class Selection extends Component {
       destination: evt.target.destination.value
     }
 
-    axios.post('http://localhost:8080/trips', newTrip)
+    axios.post('http://localhost:8080/trips/create', newTrip)
     .then((res) => {
       window.location.replace(`http://localhost:3000/#/trips/${res.data.id}`);
-      window.location.href(`http://localhost:3000/#/trips/${res.data.id}`);
+      // window.location.href(`http://localhost:3000/#/trips/${res.data.id}`);
     });
+  }
+
+
+  joinTrip = (evt) => {
+    evt.preventDefault();
+    const tripCode = {trip_id: evt.target.code.value} ;
+
+
+     axios.post(`http://localhost:8080/trips/join`, tripCode )
+      .then((res) => {
+
+
+
+        // console.log("res " , res)
+      // if(res.data.exists){
+      window.location.replace(`http://localhost:3000/#/trips/${tripCode}`);
+      window.location.href(`http://localhost:3000/#/trips/${tripCode}`);
+      // } else {
+      // window.location.replace(`http://localhost:3000/#/trips`);
+      // window.location.href(`http://localhost:3000/#/trips`);
+      // }
+    });
+
+
   }
 }
 
