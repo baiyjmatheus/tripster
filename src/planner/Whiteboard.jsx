dbdb21892d167f48914a6170f06fb6c62fdb1e55
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-01-29 00:44:33 +0000

    basic switch feature working

--- src/planner/Whiteboard.jsx
+++ src/planner/Whiteboard.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom'
+
 
 class Whiteboard extends Component {
   render() {
     return (
+    <Router>
       <main id="whiteboard" className="full-height">
         <div id="plan-status">
           <h1>Status bar</h1>
         </div>
         <hr />
+         <div>
+          <ul>
+            <li><Link to="/">About Us </Link></li>
+            <li><Link to="/flight"> flight</Link></li>
+            <li><Link to="/hotel"> hotel</Link></li>
+
+          </ul>
+        </div>
         <div>
-          < Flight />
+        <Switch>
+
+          <Route  path='/flight' component={Flight}/>
+          <Route  path='/hotel' component={Hotel}/>
+         </Switch>
         </div>
       </main>
+    </Router>
     );
+  }
+}
+
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        <h1> this is the home page </h1>
+      </div>
+    )
   }
 }
 
@@ -26,7 +57,7 @@ class Flight  extends Component {
   }
 }
 
-class Hotels  extends Component {
+class Hotel  extends Component {
   render () {
     return (
       <div>
