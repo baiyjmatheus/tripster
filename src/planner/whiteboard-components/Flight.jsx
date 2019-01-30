1ca2c113c4464b2f0089a4a0faf79226ca095f75
Matheus Bai <baiyj.matheus@gmail.com>
2019-01-30 21:17:26 +0000

    add img to each flight option

--- src/planner/whiteboard-components/Flight.jsx
+++ src/planner/whiteboard-components/Flight.jsx
@@ -22,7 +22,7 @@ class Flight  extends Component {
   render () {
     console.log(this.state.flights);
     const flightCards = this.state.flights.map((flight) => {
-          return  <Card title={`${flight.route.length - 1} stops`} rating={((flight.quality / 100) / 2).toPrecision(2)} address={`From: ${flight.flyFrom} \t To: ${flight.flyTo}`} price={flight.price}/>
+          return  <Card title={`${flight.route.length - 1} stops`} rating={((flight.quality / 100) / 2).toPrecision(2)} address={`From: ${flight.flyFrom} \t To: ${flight.flyTo}`} price={flight.price} imgSrc={'https://images.pexels.com/photos/674783/pexels-photo-674783.jpeg?cs=srgb&dl=aerial-air-air-traffic-674783.jpg&fm=jpg'}/>
     });
     return (
       <div id="flights-container">
