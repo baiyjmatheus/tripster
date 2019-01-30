cb17ddea5c74aacfd6258eb4340361cd1b5e99c8
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-01-30 16:21:54 +0000

    stashing the original api axios request

--- src/planner/whiteboard-components/Event.jsx
+++ src/planner/whiteboard-components/Event.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 class Event  extends Component {
   render () {
+    const testObj = {
+      test1: "test1",
+      test2: "test2",
+      test3: "test3"
+    }
     return (
       <div>
         <h1> this is the event page </h1>
--- src/planner/whiteboard-components/Hotel.jsx
+++ src/planner/whiteboard-components/Hotel.jsx
@@ -1,13 +1,81 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import cors from 'cors';
 
 class Hotel  extends Component {
+
+  state = {
+    posts: []
+  }
+
+  //function to get hotel information from places API
+  // getHotels(){
+  //   let config = 'AIzaSyBuSxErYyCYiLeCJqTcL9zeWYvfObimKx4'
+  //   axios
+  //     .get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1000&type=lodging&key=AIzaSyBuSxErYyCYiLeCJqTcL9zeWYvfObimKx4", {headers: config})
+  //     .then(response => {
+  //       this.setState({
+  //         posts: response.data.posts,
+  //       })
+  //     })
+  // }
+
+  componentDidMount(){
+    // this.getHotels();
+    // axios.defaults.headers.common['Authorization'] = "AIzaSyBuSxErYyCYiLeCJqTcL9zeWYvfObimKx4"
+
+    fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=McDonalds&inputtype=textquery&fields=photos&key=AIzaSyBuSxErYyCYiLeCJqTcL9zeWYvfObimKx4',
+      {method: 'GET',
+       mode: 'cors',
+
+
+       }).then((response) =>
+       console.log(response)
+       )
+       /*
+    axios.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json', {
+    headers: { 'content-type': 'application/json' },
+    crossDomain: true,
+    params: {
+      input: "McDonalds",
+      inputtype: "textquery",
+      fields: "photos",
+      key: "AIzaSyBuSxErYyCYiLeCJqTcL9zeWYvfObimKx4"
+    }
+    })
+      .then(json => console.log(json))*/
+  }
+
+
   render () {
+    // const posts = this.state.posts;
+    // const postItem = posts.map(post => {
+    //   return <div> <li> {post} </li></div>
+    // })
     return (
+     <div>
       <div>
-        <h1> this is the hotels page </h1>
+          <h1> this is the hotels page </h1>
       </div>
+
+    </div>
+
     )
   }
 }
 
 export default Hotel
+
+
+
+     /* <React.Fragment>
+        <div>
+          <h1> this is the hotels page </h1>
+        </div>
+
+        <div className ="test">
+
+           posts.map(post => (<li> {post.name} </li>))
+
+        </div>
+      </React.Fragment> */
