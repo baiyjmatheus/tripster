d1e213f791c4fcc24897487e8153696ed710bc51
Matheus Bai <baiyj.matheus@gmail.com>
2019-01-30 21:18:11 +0000

    remove comments from flight component

--- src/planner/whiteboard-components/Flight.jsx
+++ src/planner/whiteboard-components/Flight.jsx
@@ -15,12 +15,10 @@ class Flight  extends Component {
     axios.get(`http://localhost:8080/trips/${this.props.tripId}/flights`)
       .then((res) => {
         this.setState({flights: res.data.data});
-        console.log(res.data.data);
       });
   }
 
   render () {
-    console.log(this.state.flights);
     const flightCards = this.state.flights.map((flight) => {
           return  <Card title={`${flight.route.length - 1} stops`} rating={((flight.quality / 100) / 2).toPrecision(2)} address={`From: ${flight.flyFrom} \t To: ${flight.flyTo}`} price={flight.price} imgSrc={'https://images.pexels.com/photos/674783/pexels-photo-674783.jpeg?cs=srgb&dl=aerial-air-air-traffic-674783.jpg&fm=jpg'}/>
     });
