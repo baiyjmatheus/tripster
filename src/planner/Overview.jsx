4ccbcaa05526d60e74972ac0591d53daf31a5601
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-02-03 22:49:45 +0000

    made overview a child element in whiteboard

--- src/planner/Overview.jsx
+++ src/planner/Overview.jsx
@@ -3,22 +3,26 @@ import React, { Component } from 'react';
 class Overview extends Component {
   render() {
 
+    console.log(this.props.stepStat)
+
     return (
-      <aside id="summary" className="full-height">
-        <i className="fas fa-map-marker-alt"></i>
 
-        <div id="progress">
-          <ul id="steps">
-             <li><i className="fas fa-user-friends"></i></li>
-             <li><i className="fas fa-plane-departure"></i></li>
-             <li><i className="fas fa-bed"></i></li>
-             <li><i className="fas fa-dungeon"></i></li>
-             <li><i className="fas fa-ticket-alt"></i></li>
-           </ul>
+       <aside id="summary" className="full-height">
+                <i className="fas fa-map-marker-alt"></i>
+
+                  <div id="progress">
+                    <ul id="steps">
+                       <li><i className="fas fa-user-friends"></i></li>
+                       <li><i className="fas fa-plane-departure"></i></li>
+                       <li><i className="fas fa-bed"></i></li>
+                       <li><i className="fas fa-dungeon"></i></li>
+                       <li><i className="fas fa-ticket-alt"></i></li>
+                     </ul>
 
 
-        </div>
-      </aside>
+                  </div>
+        </aside>
+
     );
   }
 }
--- src/planner/Whiteboard.jsx
+++ src/planner/Whiteboard.jsx
@@ -13,6 +13,7 @@ import Flight from './whiteboard-components/Flight.jsx';
 import Hotel from './whiteboard-components/Hotel.jsx';
 import Event from './whiteboard-components/Event.jsx';
 import Attraction from './whiteboard-components/Attraction.jsx';
+import Overview from './Overview.jsx'
 
 class Whiteboard extends Component {
   constructor() {
@@ -67,7 +68,7 @@ class Whiteboard extends Component {
     } else {
       return (
 
-        <Router>
+         <Router>
           <main id="whiteboard" className="full-height">
             <div id="trip-header">
               <div id="trip-info">
@@ -78,56 +79,59 @@ class Whiteboard extends Component {
                 <Ready status={this.state.readyBtnStatus} currentStep={this.state.currentStep} changeStepState={this.changeStepState} changeReadyBtn={this.changeReadyBtn} color={this.state.readyBtnColor}/>
               </div>
             </div>
+
+            <Overview stepStatus = {this.props.match} />
+
             <div id="suggestion-container">
               <Switch>
-                <Route 
-                  exact path={`${url}`} 
+                <Route
+                  exact path={`${url}`}
                   render={() => <Start
-                    tripURL={this.props.tripURL} 
-                    currentStep={this.state.currentStep} 
-                  />} 
-                />
-                <Route 
-                  path={`${url}/flights`} 
-                  render={() => <Flight 
-                    tripId={this.props.tripId} 
-                    socket={this.props.socket} 
-                    tripURL={this.props.tripURL} 
-                    currentStep={this.state.currentStep}
-                    getSelectedItems={this.getSelectedItems}
-                    currentUser={this.props.currentUser}
-                  />}
-                />
-                <Route 
-                  path={`${url}/hotels`}  
-                  render={() => <Hotel 
-                    socket={this.props.socket} 
-                    tripId={this.props.tripId} 
-                    tripURL={this.props.tripURL} 
-                    currentStep={this.state.currentStep}
-                    getSelectedItems={this.getSelectedItems}
-                    currentUser={this.props.currentUser}
-                  />}
-                />
-                <Route 
-                  exact path={`${url}/events`} 
-                  render={() => <Event 
-                    url={url} 
-                    tripId={this.props.tripId} 
                     tripURL={this.props.tripURL}
-                    socket={this.props.socket} 
+                    currentStep={this.state.currentStep}
+                  />}
+                />
+                <Route
+                  path={`${url}/flights`}
+                  render={() => <Flight
+                    tripId={this.props.tripId}
+                    socket={this.props.socket}
+                    tripURL={this.props.tripURL}
+                    currentStep={this.state.currentStep}
+                    getSelectedItems={this.getSelectedItems}
+                    currentUser={this.props.currentUser}
+                  />}
+                />
+                <Route
+                  path={`${url}/hotels`}
+                  render={() => <Hotel
+                    socket={this.props.socket}
+                    tripId={this.props.tripId}
+                    tripURL={this.props.tripURL}
+                    currentStep={this.state.currentStep}
+                    getSelectedItems={this.getSelectedItems}
+                    currentUser={this.props.currentUser}
+                  />}
+                />
+                <Route
+                  exact path={`${url}/events`}
+                  render={() => <Event
+                    url={url}
+                    tripId={this.props.tripId}
+                    tripURL={this.props.tripURL}
+                    socket={this.props.socket}
                     currentUser={this.props.currentUser}
                     currentStep={this.state.currentStep}
                     getSelectedItems={this.getSelectedItems}
                   />}
                 />
-                <Route 
-                  path={`${url}/attractions`}  
-                  render={() => <Attraction 
-                    url={url} 
-                    tripId={this.props.tripId} 
+                <Route
+                  path={`${url}/attractions`}
+                  render={() => <Attraction
+                    url={url}
+                    tripId={this.props.tripId}
                     tripURL={this.props.tripURL}
-                    socket={this.props.socket} 
+                    socket={this.props.socket}
                     currentUser={this.props.currentUser}
                     currentStep={this.state.currentStep}
                     getSelectedItems={this.getSelectedItems}
@@ -136,7 +140,7 @@ class Whiteboard extends Component {
               </Switch>
             </div>
           </main>
-        </Router>  
+        </Router>
       );
     }
   }
