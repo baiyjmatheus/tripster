2d936b1c489a87bd6cf070fad3c2bfb06bbb43c6
Matheus Bai <baiyj.matheus@gmail.com>
2019-02-05 18:13:33 +0000

    chat header and notifications implemented

--- src/planner/ChatHeader.jsx
+++ src/planner/ChatHeader.jsx
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 
 class ChatHeader extends Component {
   render() {
+    const users = this.props.users.map((userColor) => {
+      return (
+        <div>
+          <img className="avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt="" style={{backgroundColor: userColor}} />
+        </div>
+      );
+    });
     return (
       <section id="users">
         <div id="users-icons">
-          <div>
-            <img className="avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt=""/>
-          </div>
-          <div>
-            <img className="avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt=""/>
-          </div>
-          <div>
-            <img className="avatar" src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png" alt=""/>
-          </div>
+          {users}
         </div> 
       </section>
     );
--- src/planner/MessageList.jsx
+++ src/planner/MessageList.jsx
@@ -5,13 +5,9 @@ class MessageList extends Component {
   render() {
     return (
       <section id="message-list"> 
-        <Message color={"tomato"} name={"Mike"} content={"JUSTIFY MORE CONTENT!"}/>
-        <Message color={"greenyellow"} name={"Matt"} content={"BURRITO GENIUS"}/>
-        <Message color={"yellow"} name={"Mariam"} content={"WHAT? I DON'T KNOW"}/>
-
-		{              	
-      		this.props.messages.map(msg => <Message color={ msg.color } name= { msg.name } content={msg.content}/> )
-      	}
+        {              	
+          this.props.messages.map(msg => <Message color={ msg.color } name= { msg.name } content={msg.content}/> )
+        }
       </section>
     );
   }
