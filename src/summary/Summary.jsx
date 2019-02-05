5bec6dc58ab0e08167799649cd95d07368ae0390
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-02-05 01:11:20 +0000

    finished rendering content of summary page

--- src/summary/Summary.jsx
+++ src/summary/Summary.jsx
@@ -34,6 +34,14 @@ class Summary extends Component {
       return `${day}-${month}-${year}`
     }
 
+    const returnBestOption = (array)=> {
+      const sortedArray = array.sort(function (a, b) {
+        return a.price - b.price;
+      });
+
+      return sortedArray[0]
+    }
+
     console.log("this is the date from query: ", this.state.data)
 
     if (this.state.data) {
@@ -46,24 +54,52 @@ class Summary extends Component {
           const eventsList = this.state.data.events.map(event => {
             console.log('attraction from map:', event.name)
                return  (<li> {event.name} </li>)
+          })
+
+          const flightsList = this.state.data.flights.map(flight => {
+              const routeList = flight.route.map(layover => {
+                return(<li>{layover.cityFrom} - {layover.cityTo} </li>)
+              })
+              return  (<ul> <li>flight id# {flight.id}: {(flight.route.length - 1)} stops - ${flight.price} </li>
+                {routeList}
+                </ul>)
+
+          })
+
+          const hotelsList = this.state.data.hotels.map(hotel => {
+                return  (<li> {hotel.name} - ${hotel.price}/night </li>)
            })
+
+          const bestFlightOption = returnBestOption(this.state.data.flights)
+          const bestHotelOption = returnBestOption(this.state.data.hotels)
       return (
       <div id='summary-container'>
         <section id='summary-details'>
 
           <h1> trip summary </h1>
-          <div> <h3> Trip Name : {this.state.data.trip[0].name} </h3> </div>
-          <h4> From {formattedDate(this.state.data.trip[0].start_date)} until {formattedDate(this.state.data.trip[0].end_date)} </h4>
-          <h4> Origin: {this.state.data.trip[0].origin}</h4>
-          <h4> Destination: {this.state.data.trip[0].destination}</h4>
-
-          <div> <h3> Flight details </h3></div>
-          <div> <h3> Chosen Events:  </h3>
-             <ul> {eventsList}</ul>
+         <div id='trip-details'>
+            <h3> Trip Name : {this.state.data.trip[0].name} </h3>
+            <h4> From {formattedDate(this.state.data.trip[0].start_date)} until {formattedDate(this.state.data.trip[0].end_date)} </h4>
+            <h4> Origin: {this.state.data.trip[0].origin}</h4>
+            <h4> Destination: {this.state.data.trip[0].destination}</h4>
           </div>
-          <div> <h3> Chosen Attractions: </h3>
+
+          <div>
+            <h3> Flight options </h3>
+            <ul> {flightsList} </ul>
+            <li> Our reccommended flight: {bestFlightOption.id} - ${bestFlightOption.price} </li>
+
+              <h3> Hotel options </h3>
+            <ul> {hotelsList} </ul>
+             <li> Our reccommended hotel: {bestHotelOption.name} - ${bestHotelOption.price}/night </li>
+
+           <h3> Chosen Events:  </h3>
+             <ul> {eventsList}</ul>
+
+          <h3> Chosen Attractions: </h3>
           <ul> {attractionsList}</ul>
           </div>
+
 
         </section>
         <MapContainer trip={this.state.data}/>
