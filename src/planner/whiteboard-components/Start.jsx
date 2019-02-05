9daf4dd580b1557099536e6cd6b34c77e8e35216
MikeEngerer <michael.engerer@hotmail.com>
2019-02-05 19:02:23 +0000

    added welcome message and trip code link

--- src/planner/whiteboard-components/Start.jsx
+++ src/planner/whiteboard-components/Start.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
 class Start extends Component {
+
+
   render() {
     if (this.props.currentStep !== 'start') {
       return (
@@ -9,11 +11,23 @@ class Start extends Component {
       );
     } else {
       return (
-        <div id="start">
+        <div id="start" style={{color: '#5078f2'}}>
+          <h2>Invite your friends!</h2>
+          <div style={{display: 'flex', justifyContent: 'space-between', width: '280px', margin: '0 auto'}}>
+            <span> Trip code:&nbsp;</span>
+            <input readOnly value={`${this.props.tripId}`} style={{border: 'none', color: '#5078f2'}} id="copy" />
+            <button onClick={this.copyToClipboard}><i style={{color: '#5078f2'}} className="fas fa-copy"></i></button>
+          </div>
           <h2>Click ready and wait for all participants</h2>
         </div>
       );
     }
+  }
+
+  copyToClipboard = (e) => {
+    let link = document.getElementById('copy')
+    link.select()
+    document.execCommand('copy')
   }
 }
 
